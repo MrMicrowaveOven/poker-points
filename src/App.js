67cf09063bcbf33a457f6355a747161d2bd8372e
mrmicrowaveoven <benj@zagorski.com>
2023-08-11 01:38:41 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import {useState} from 'react'
 import Table from './Table.js'
-import Name from './Name.js'
 import Input from './Input.js'
 import './App.css';
 
 function App() {
   const [selectedCard, selectCard] = useState(null)
-  const names = () => {
-    return [...Array(10).keys()].map(num => {
-      return  <Name
-                index={num + 1}
-                name={"Benji"}
-              />
-    })
-  }
+
   return (
     <div className="app">
       <Table selectedCard={selectedCard}>
-        {names()}
       </Table>
       <Input 
         displaySelectedCard={(num) => selectCard(num)}
--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 import Card from './Card.js'
+import Name from './Name.js'
 import './Table.css';
 import './cardsOnTable.css'
 
 const Table = (props) => {
     const [revealed, reveal] = useState(false)
+    
     const cardsOnTable = [...Array(10).keys()].map(num => {
         const ownCard = num === 0
         return (<div className={"card-on-table card-on-table" + (num + 1)}>
             <Card faceDown={!revealed} number={props.selectedCard} ownCard={ownCard} key={num}/>
         </div>)
     })
+
+    const names = () =>
+        [...Array(10).keys()].map(num =>
+            <Name
+                index={num + 1}
+                name={"Benji"}
+            />
+        )
+
     return (
         <div className="table">
-            {props.children}
+            {names()}
             <button onClick={() => reveal(true)} className="reveal-button">Reveal!</button>
             {cardsOnTable}
         </div>
