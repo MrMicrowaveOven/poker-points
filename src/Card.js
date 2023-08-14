6faf5d36e008cba16b9b830b2310183ab35ed75b
mrmicrowaveoven <benj@zagorski.com>
2023-08-14 15:22:13 -0500

    Now use clubs notation

--- src/Card.js
+++ src/Card.js
@@ -1,24 +1,42 @@
-import one from './cards/ac.webp'
-import two from './cards/2c.webp'
-import three from './cards/3c.webp'
-import five from './cards/5c.webp'
-import eight from './cards/8c.webp'
-import king from './cards/kc.webp'
+import oneClubs from './cards/ac.webp'
+import twoClubs from './cards/2c.webp'
+import threeClubs from './cards/3c.webp'
+import fiveClubs from './cards/5c.webp'
+import eightClubs from './cards/8c.webp'
+import kingClubs from './cards/kc.webp'
+import oneDiamonds from './cards/ad.webp'
+import twoDiamonds from './cards/2d.webp'
+import threeDiamonds from './cards/3d.webp'
+import fiveDiamonds from './cards/5d.webp'
+import eightDiamonds from './cards/8d.webp'
+import kingDiamonds from './cards/kd.webp'
+import oneHearts from './cards/ah.webp'
+import twoHearts from './cards/2h.webp'
+import threeHearts from './cards/3h.webp'
+import fiveHearts from './cards/5h.webp'
+import eightHearts from './cards/8h.webp'
+import kingHearts from './cards/kh.webp'
+import oneSpades from './cards/as.webp'
+import twoSpades from './cards/2s.webp'
+import threeSpades from './cards/3s.webp'
+import fiveSpades from './cards/5s.webp'
+import eightSpades from './cards/8s.webp'
+import kingSpades from './cards/ks.webp'
 import faceDownCard from './cards/faceDown.jpeg'
 
 const Card = (props) => {
     const {number, select, faceDown, ownCard} = props
     const cardMap = [
         null,
-        one,
-        two,
-        three,
+        oneClubs,
+        twoClubs,
+        threeClubs,
         null,
-        five,
+        fiveClubs,
         null, null,
-        eight,
+        eightClubs,
         null,null,null,null,
-        king
+        kingClubs
     ]
     let className = "card"
     if (props.selected) className += " card-selected"
