13ee17001bef148c4232df9cd6ec8c3583acfaa5
mrmicrowaveoven <benj@zagorski.com>
2023-08-14 13:58:11 -0500

    Added AblyModule

---
+++ src/AblyModule.js
@@ -0,0 +1,3 @@
+export const subscribe = () => {
+    console.log("FOUND")
+}
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {useState} from 'react'
 import Table from './Table.js'
 import Input from './Input.js'
+import { subscribe } from './AblyModule.js'
 import './App.css';
 
 function App() {
   const [selectedCard, selectCard] = useState(null)
+  subscribe()
 
   return (
     <div className="app">
