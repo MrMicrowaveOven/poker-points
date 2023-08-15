7926de542b1c72e8c4cfd17c50b97d8826850844
mrmicrowaveoven <benj@zagorski.com>
2023-08-14 22:29:03 -0500

    Now assigns number correctly, shows view with current player at bottom

--- src/App.js
+++ src/App.js
@@ -8,23 +8,18 @@ import './App.css';
 
 function App() {
   configureAbly({key: process.env['REACT_APP_ABLY_AUTH_KEY']})
-  const [cardState, setCardState] = useState([])
-  const [playerState, setPlayerState] = useState([])
+  const emptyArray = Array.apply(null, Array(10)).map(function () {})
+  const [cardState, setCardState] = useState(emptyArray)
+  const [playerState, setPlayerState] = useState(emptyArray)
   const [playerChannel] = useChannel("players", (state) => setPlayerState(state.data))
   const [cardChannel] = useChannel("cards", (state) => setCardState(state.data))
   const [selectedCard, setSelectedCard] = useState(null)
   const [playerName, setPlayerName] = useState(null)
   const [playerNumber, setPlayerNumber] = useState(null)
 
-  console.log(playerState)
-  console.log(cardState)
-
   useEffect(() => {
-    assignPlayerNumber()
-  }, [])
-
-  const assignPlayerNumber = () => {
-    const numNulls = 10 - playerState.filter(String).length
+    // Assign player number
+    const numNulls = playerState.filter(n => !n).length
     const rand = Math.floor(Math.random() * numNulls)
     let nullIndex = 0
 
@@ -37,7 +32,11 @@ function App() {
         nullIndex++
       }
     }
-  }
+    // Get player name
+    // const givenName = prompt("What's your name?")
+    const givenName = "Benji"
+    setPlayerName(givenName)
+  }, [])
 
   const updateCurrentPlayerState = (name) => {
     const currentPlayerState = playerState
@@ -59,9 +58,31 @@ function App() {
     updateCurrentCardState(selectedCard)
   }, [selectedCard])
 
+  const getPlayerStateView = () => {
+    const playerStateView = [...playerState]
+    for(let i = 0; i < playerNumber; i++) {
+      playerStateView.push(playerStateView.shift())
+    }
+    return playerStateView
+  }
+  const playerStateView = getPlayerStateView()
+
+  const getCardStateView = () => {
+    const cardStateView = [...cardState]
+    for(let i = 0; i < playerNumber; i++) {
+      cardStateView.push(cardStateView.shift())
+    }
+    return cardStateView
+  }
+  const cardStateView = getCardStateView()
+
   return (
     <div className="app">
-      <Table selectedCard={selectedCard} names={playerState}>
+      <Table
+        selectedCard={selectedCard}
+        names={playerStateView}
+        cards={cardStateView}
+      >
       </Table>
       <Input
         displaySelectedCard={(num) => setSelectedCard(num)}
